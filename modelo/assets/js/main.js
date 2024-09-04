a6ca0ee6a18eaf7f3b715f5fe01771c12af4438f
MauricioIF <Mauricio.santos@escolar.ifrn.edu.br>
2024-09-04 01:04:10 -0300

    concluido o IMC

--- modelo/assets/js/main.js
+++ modelo/assets/js/main.js
@@ -4,22 +4,23 @@ function defImc() {
 
   function gerarImc(evento) {
     evento.preventDefault();
+    const nome = form.querySelector(".nome")
     const peso = parseFloat(form.querySelector(".peso").value);
     const altura = parseFloat(form.querySelector(".altura").value);
     const imc = peso / (altura * altura);
 
     if (imc < 18.5) {
-      resultado.innerHTML += `<p>Seu imc é: ${imc.toFixed(2)} (Abaixo do peso)</p>`;
+      resultado.innerHTML += `<p>${nome.value}, seu imc é: ${imc.toFixed(2)} (Abaixo do peso)</p>`;
     } else if (imc >= 18.5 && imc <= 24.9 ){
-      resultado.innerHTML +=  `<p>Seu imc é: ${imc.toFixed(2)} (Peso normal)</p>`
+      resultado.innerHTML +=  `<p>${nome.value}, seu imc é: ${imc.toFixed(2)} (Peso normal)</p>`
     } else if (imc >= 25 && imc <= 29.9){
-       resultado.innerHTML +=  `<p>Seu imc é: ${imc.toFixed(2)} (sobrepeso)</p>`
+       resultado.innerHTML +=  `<p>${nome.value}, seu imc é: ${imc.toFixed(2)} (sobrepeso)</p>`
     } else if (imc >= 30 && imc <= 34.9){
-      resultado.innerHTML +=  `<p>Seu imc é: ${imc.toFixed(2)} (obesidade grau 1)</p>`
+      resultado.innerHTML +=  `<p>${nome.value}, seu imc é: ${imc.toFixed(2)} (obesidade grau 1)</p>`
    }  else if (imc >= 35 && imc <= 39.9){
-      resultado.innerHTML +=  `<p>Seu imc é: ${imc.toFixed(2)} (obesidade grau 2)</p>`
+      resultado.innerHTML +=  `<p>${nome.value}, seu imc é: ${imc.toFixed(2)} (obesidade grau 2)</p>`
   } else {
-    resultado.innerHTML +=  `<p>Seu imc é: ${imc.toFixed(2)} (obesidade grau 3)</p>`
+    resultado.innerHTML +=  `<p>${nome.value}, seu imc é: ${imc.toFixed(2)} (obesidade grau 3)</p>`
   }}
   form.addEventListener("submit", gerarImc);
 }
